95a4cf7eb35dba0bcbbeda18a06c8895d6011064
Elijah Ryder <ezryder2001@gmail.com>
2016-11-08 13:05:46 +0000

    Adding SUperComputer

---
+++ SuperComputer.java
@@ -0,0 +1,22 @@
+//Author: Elijah Ryder
+//Date: Halloween, 2016
+//Description: make two loops 
+
+public class SuperComputer {
+    public static void main(String []args) {
+        
+        //int x = 5;
+        //System.out.println("x == " + x);
+        //int x = 1;
+        
+            
+        for( int i = 0 ; i <= 20 ; i+=2 ) {
+            System.out.print(i + " ");
+        }  
+        System.out.println();
+    
+        //for (int i = 10;  i<= 0; i --) {
+          //   System.out.print(i + " ");
+    }
+    
+}
