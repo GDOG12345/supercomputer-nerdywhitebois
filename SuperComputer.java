d90f00b07e0a1e9c6fb4e21b87543903e8a391d3
GDOG12345 <19MutzG@bpcsd.org>
2016-11-09 08:00:50 -0500

    Update SuperComputer.java

--- SuperComputer.java
+++ SuperComputer.java
@@ -37,6 +37,103 @@ public class SuperComputer {
             }
             System.out.println();
         }
+        // author: Gavyn Mutz
+// Date: 10/31/16
+// description: making two loops
+
+import java.util.Scanner;
+public class Test {
+    public static void main (String[] args) {
+        
+        /*
+        int x = 5;
+        System.out.println("x == " + x);
+        x = x + 1;
+        */
+        
+        // print what loop do you want? 
+        // 1. loop from 0 to 20 counting by 2
+        // 2. loop from 15 to 0 counting down by 1
+        // collect using a scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("welcome to my looping program.");
+       
+        System.out.println("1. Loop from 0 to 20 counting by 2");
+        
+        System.out.println("2. Loop from 15 to 0 counting down by 1");
+       
+        System.out.println("please enter your choice here: ");
+        String choice =scanner.nextLine();
+       
+        if (choice.equals("1")) {
+        
+            for (int i = 0 ; i <= 20; i+=2) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        else if (choice.equals("2")) {
+        
+            for (int i = 15; i >= 0; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            
+            
+        }
+        
+    }
+    
+// author: Gavyn Mutz
+// Date: 10/31/16
+// description: making two loops
+
+import java.util.Scanner;
+public class Test {
+    public static void main (String[] args) {
+        
+        /*
+        int x = 5;
+        System.out.println("x == " + x);
+        x = x + 1;
+        */
+        
+        // print what loop do you want? 
+        // 1. loop from 0 to 20 counting by 2
+        // 2. loop from 15 to 0 counting down by 1
+        // collect using a scanner
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("welcome to my looping program.");
+       
+        System.out.println("1. Loop from 0 to 20 counting by 2");
+        
+        System.out.println("2. Loop from 15 to 0 counting down by 1");
+       
+        System.out.println("please enter your choice here: ");
+        String choice =scanner.nextLine();
+       
+        if (choice.equals("1")) {
+        
+            for (int i = 0 ; i <= 20; i+=2) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        else if (choice.equals("2")) {
+        
+            for (int i = 15; i >= 0; i--) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            
+            
+        }
+        
+    }
+    
+
 
     }
 
